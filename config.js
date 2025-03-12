dc33cd429edcc2019da1f1970b5cf9990592367f
rbsriram <rbsriram@gmail.com>
2025-03-12 20:26:01 +0400

    commit

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
-// Configuration settings for mjiva landing page
 const CONFIG = {
     // Form submission settings
     form: {
         email: "mjiva.app@gmail.com",        // Email to receive notifications
-        googleSheetId: "1QfLO_EZVurxDGAbJFYVtjH48wJxabtdSdOgqZ5SbHEFd20rLnvdvFlMt",  // ID from Google Sheets URL
+        googleSheetId: "1bEhYutcpEwcxyTADfcJBI1d6h0M5f_79l6yamNvShxk",  // ID from Google Sheets URL
         thankYouPage: "thanks.html",            // Thank you page URL (relative or absolute)
-        subject: "With love from mjiva - You are in!"            // Email subject line
+        subject: "New mjiva Signup!",           // Email subject line
+        googleFormId: "1FAIpQLScA3D8Fq0lPyQw1Ve9xnz2jI0mKjDxV_vQp8jRsfh9tzrkqaA",   // Google Form ID from the URL
+        googleFormEntryId: "1020424641"             // Entry ID from the pre-filled URL
     },
     
     // Launch date settings (format: month day, year hour:minute:second)
